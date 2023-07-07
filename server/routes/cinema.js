ed1aa4aaeecc6b6f8a40729241ac52a9a7f4f3c4
tqx7514 <tqx7514@naver.com>
2023-07-07 09:21:15 +0900

    230707 merge ljy

--- server/routes/cinema.js
+++ server/routes/cinema.js
@@ -2,6 +2,6 @@ const express = require("express");
 const router = express.Router();
 const { Region, Cinema } = require("../controllers/cinema");
 
-router.get("/region", Region);
+// router.get("/region", Region);
 
 module.exports = router;

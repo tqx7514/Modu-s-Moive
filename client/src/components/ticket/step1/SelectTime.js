4c0ba8ef007a1d2833660c465b6231820f569a82
kdhun1838 <kdhun0117@gmail.com>
2023-07-13 18:48:07 +0900

    기존코드

--- client/src/components/ticket/step1/SelectTime.js
+++ client/src/components/ticket/step1/SelectTime.js
@@ -40,7 +40,7 @@ const ScheduleBtn = styled.div`
   width: 23%;
   margin-left: 2%;
   margin-bottom: 10px;
-  &.selected {
+  &.selected{
     border: 1px solid;
   }
   &:nth-child(4n + 1) {
@@ -66,177 +66,151 @@ const ScheduleBtn = styled.div`
 
 const SelectTime = () => {
   const [selectedFilter, setSelectedFilter] = useState("전체");
-  const [selectedTime, setSelectedTime] = useState(null);
-  console.log('selectedTimeBtn?????????', selectedTime);
+  const [selectedTime, setSelectedTime] = useState("");
 
   const handleFilter = (start) => {
     setSelectedFilter(start);
   };
 
   const { data, time } = useSelector(({ stepfirst }) => stepfirst);
-  console.log("321321321", data.time);
+  console.log(data);
 
   const dispatch = useDispatch();
 
-  const onSelectedTime = useCallback(
-    (cinema, movie_name, start, end) => {
-      dispatch(setTimeData({ cinema, movie_name, start, end }));
+  const onSelctedTime = useCallback(
+    (time) => {
+      dispatch(setTimeData(time));
     },
     [dispatch]
   );
 
-  const handleSelectTime = (index, item, start) => {
-    console.log("handleSelectTime->", index);
-    setSelectedTime(
-      item.movietimes_num
-    );
-
-    onSelectedTime(item.cinema, item.movie_name, start, item.end);
+  const handleSelectTime = (e, start, end) => {
+    setSelectedTime(e.target.start);
+    onSelctedTime(start, end);
+    console.log(selectedTime);
   };
-
+  
   useEffect(() => {
     dispatch(readTime());
-  }, [dispatch]);
+  }, []);
 
   const groupedData = {};
-  console.log("SelectTime->", time)
 
   time.forEach((item) => {
-    const {
-      movietimes_num,
-      cinema,
-      movie_name,
-      age,
-      disp,
-      language,
-      start,
-      end,
-      seat,
-    } = item;
+    const { cinema, movie_name, age, disp, language, start, end, seat } = item;
     const key = `${movie_name}-${disp}-${language}`;
     if (!groupedData[key]) {
       groupedData[key] = {
-        movietimes_num,
         cinema,
         movie_name,
         age,
         disp,
         language,
         start: [],
-        end: [],
+        end,
         seat,
       };
     }
     groupedData[key].start.push(start);
-    groupedData[key].end.push(end);
   });
 
   const uniqueTimes = Object.values(groupedData);
 
   const filteredTimes = uniqueTimes
     .filter((item) => {
-
-      console.log("SelectTime filter  0>", item)
-      if (data.movie) {
-        return (
-          item.cinema === data.cinema &&
-          item.movie_name === data.movie.movie_name
+      if (selectedFilter === "전체") {
+        return true;
+      } else if (selectedFilter === "스페셜관") {
+        return item.disp === "스페셜관";
+      } else if (selectedFilter === "Atmos") {
+        return item.disp === "Atmos";
+      } else if (selectedFilter === "13시 이후") {
+        return item.start.some(
+          (start) => parseInt(start.split(":")[0], 10) >= 13
         );
-      } else {
-        return item.cinema === data.cinema;
+      } else if (selectedFilter === "19시 이후") {
+        return item.start.some(
+          (start) => parseInt(start.split(":")[0], 10) >= 19
+        );
+      } else if (selectedFilter === "심야") {
+        return item.start.some((start) => {
+          const hour = parseInt(start.split(":")[0], 10);
+          return (hour >= 0 && hour < 6) || hour === 23;
+        });
       }
+      return true;
     })
-    .filter((item) => {
-      console.log("SelectTime filter  1>", item)
-
-      switch (selectedFilter) {
-        case "전체":
-          return true;
-        case "스페셜관":
-          return item.disp === "스페셜관";
-        case "Atmos":
-          return item.disp === "Atmos";
-        case "13시 이후":
-          return item.start.some((start) => {
-            const hour = parseInt(start.split(":")[0]);
-            return hour >= 13;
-          });
-        case "19시 이후":
-          return item.start.some((start) => {
-            const hour = parseInt(start.split(":")[0]);
-            return hour >= 19;
-          });
-        case "심야":
-          return item.start.some((start) => {
-            const hour = parseInt(start.split(":")[0]);
-            return hour >= 0 && hour < 5;
-          });
-        default:
-          return true;
-      }
+    .map((item) => {
+      const { start } = item;
+      const filteredStart = start.filter((time) => {
+        if (selectedFilter === "13시 이후") {
+          return parseInt(time.split(":")[0], 10) >= 13;
+        } else if (selectedFilter === "19시 이후") {
+          return parseInt(time.split(":")[0], 10) >= 19;
+        } else if (selectedFilter === "심야") {
+          const hour = parseInt(time.split(":")[0], 10);
+          return (hour >= 0 && hour < 6) || hour === 23;
+        }
+        return true;
+      });
+      return { ...item, start: filteredStart };
     });
-
-    console.log("SelectTime filteredTimes  1>", filteredTimes)
 
   return (
     <>
       <BtnWrap>
         <FilterBtn
-          onClick={() => handleFilter("전체")}
+          onClick={handleFilter}
           className={selectedFilter === "전체" ? "selected" : ""}
         >
           전체
         </FilterBtn>
         <FilterBtn
-          onClick={() => handleFilter("스페셜관")}
+          onClick={handleFilter}
           className={selectedFilter === "스페셜관" ? "selected" : ""}
         >
           스페셜관
         </FilterBtn>
         <FilterBtn
-          onClick={() => handleFilter("Atmos")}
+          onClick={handleFilter}
           className={selectedFilter === "Atmos" ? "selected" : ""}
         >
           Atmos
         </FilterBtn>
         <FilterBtn
-          onClick={() => handleFilter("13시 이후")}
+          onClick={handleFilter}
           className={selectedFilter === "13시 이후" ? "selected" : ""}
         >
           13시 이후
         </FilterBtn>
         <FilterBtn
-          onClick={() => handleFilter("19시 이후")}
+          onClick={handleFilter}
           className={selectedFilter === "19시 이후" ? "selected" : ""}
         >
           19시 이후
         </FilterBtn>
         <FilterBtn
-          onClick={() => handleFilter("심야")}
+          onClick={handleFilter}
           className={selectedFilter === "심야" ? "selected" : ""}
         >
           심야
         </FilterBtn>
       </BtnWrap>
       <TimeWrap>
-      {data.cinema && data.date && (
+        {data.cinema && data.date && (
           <>
-            {filteredTimes.map((item) => {
-              console.log("SelectTime fileTime 0->", item);
-              const { start } = item;
-              const filteredStart = start.filter((time) => {
-                if (selectedFilter === "13시 이후") {
-                  return parseInt(time.split(":")[0], 10) >= 13;
-                } else if (selectedFilter === "19시 이후") {
-                  return parseInt(time.split(":")[0], 10) >= 19;
-                } else if (selectedFilter === "심야") {
-                  const hour = parseInt(time.split(":")[0], 10);
-                  return (hour >= 0 && hour < 6) || hour === 23;
+            {filteredTimes
+              .filter((item) => {
+                if (data.movie) {
+                  return (
+                    item.cinema === data.cinema &&
+                    item.movie_name === data.movie.movie_name
+                  );
+                } else {
+                  return item.cinema === data.cinema;
                 }
-                return true;
-              });
-              return { ...item, start: filteredStart };
-            }).map((item) => (
+              })
+              .map((item) => (
                 <AreaItem
                   key={`${item.cinema}-${item.movie_name}-${item.age}-${item.disp}-${item.language}`}
                   className="movie_list time"
@@ -263,15 +237,17 @@ const SelectTime = () => {
                       <p>{item.language}</p>
                     </div>
                     <div>
-                      { Object.entries(item).map((it, i) =>(
-
-                        <div>{it.map((it2, i)=>( 
-                          it2[5]
-                          ))}  
-                        </div>
-
-                        ))}
-                      
+                      {item.start.map((start, index) => (
+                        <ScheduleBtn
+                          key={index}
+                          onClick={(e) => handleSelectTime(e, start, item.end)}
+                          className={selectedTime ? 'selected' : ''}
+                        >
+                          <strong>{start}</strong>
+                          <span>{item.seat}</span>
+                          <span>{item.end}</span>
+                        </ScheduleBtn>
+                      ))}
                     </div>
                   </div>
                 </AreaItem>

144a392ce12c309c3529e53dec3d82a0df8929ab
weise5329 <austria-ra@hanmail.net>
2023-07-18 18:49:07 +0900

    AdminEvent 컴포넌트, 컨테이너, 페이지 추가 및 수정 중

---
+++ client/src/components/common/admin/AdminEventBoardListCompots.js
@@ -0,0 +1,17 @@
+// import React from "react";
+// import AdminEventItemCompots from "./AdminEventItemCompots";
+// import styled from "styled-components";
+
+// const AdminEventBoardListContainer = styled.div``;
+
+// const AdminEventBoardList = ({ events }) => {
+//   return (
+//     <AdminEventBoardListContainer>
+//       {events.map((event) => (
+//         <AdminEventItemCompots key={event.id} event={event} />
+//       ))}
+//     </AdminEventBoardListContainer>
+//   );
+// };
+
+// export default AdminEventBoardList;
--- client/src/pages/admin/AdminEventPage.js
+++ client/src/pages/admin/AdminEventPage.js
@@ -1,45 +1,36 @@
 // import React from "react";
-// import AdminEventTopCompots from "../../components/common/admin/AdminEventTopCompots";
-// import AdminEventMenuCompots from "../../components/common/admin/AdminEventMenuCompots";
-// import AdminEventBoardCompots from "../../components/common/admin/AdminEventBoardCompots";
+// import AdminEventContainer from "../../containers/common/admin/AdminEventContainer";
+// import AdminEventForm from "../../components/common/admin/AdminEventFormCompots";
+// import AdminEventBoardDetails from "../../components/common/admin/AdminEventBoardDetailsCompots";
+// import AdminEventSearch from "../../components/common/admin/AdminEventSearchCompots";
+// import AdminEventPagination from "../../components/common/admin/AdminEventBoardPaginationCompots";
 // import styled from "styled-components";
 
-// const AdminEventPageContainer = styled.div` 
+// const AdminEventPageTopBlock = styled.div``;
 
-// `;
+// const AdminEventPageMidBlock = styled.div``;
 
+// const AdminEventPageBtmBlock = styled.div``;
 
 // const AdminEventPage = () => {
-//     const categories = [
-//         { id: 1, name: "영화" },
-//         { id: 2, name: "제휴/할인" },
-//         { id: 3, name: "기타" },
-//     ];
-//     const [selectedCategory, setSelectedCategory] = React.useState(0);
-//     const [sort, setSort] = React.useState("views_desc");
-
-//     const handleSelectCategory = (categoryId) => {
-//         setSelectedCategory(categoryId);
-//     };
-
-//     const handleSortChange = (sortOption) => {
-//         setSort(sortOption);
-//     };
-
-//     return (
-//         <AdminEventPageContainer>
-//             <h1>이벤트 페이지</h1>
-//             <AdminEventTopCompots
-//             categories={categories}
-//             selectedCategory={selectedCategory}
-//             onSelectCategory={handleSelectCategory}
-//             onSortChange={handleSortChange}
-//             />
-//             <AdminEventMenuCompots />
-//             <AdminEventBoardCompots />
-//         </AdminEventPageContainer>
-        
-//     );
+//   return (
+//     <AdminEventPageTopBlock>
+//       <AdminEventContainer>
+//         <AdminEventPageBtmBlock>
+//           <AdminEventForm />
+//         </AdminEventPageBtmBlock>
+//       </AdminEventContainer>
+//       <AdminEventPageMidBlock>
+//         <AdminEventBoardDetails />
+//       </AdminEventPageMidBlock>
+//       <AdminEventPageMidBlock>
+//         <AdminEventSearch />
+//       </AdminEventPageMidBlock>
+//       <AdminEventPageMidBlock>
+//         <AdminEventPagination />
+//       </AdminEventPageMidBlock>
+//     </AdminEventPageTopBlock>
+//   );
 // };
 
 // export default AdminEventPage;

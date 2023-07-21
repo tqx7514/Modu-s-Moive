d6274b40912b949b41c8eb7f7724cc9fb519bd0a
SonGangMin <skm0628@naver.com>
2023-07-21 17:38:57 +0900

    마이페이지 하단부분 하는중

--- client/src/components/mypage/MyPageMain.js
+++ client/src/components/mypage/MyPageMain.js
@@ -1,11 +1,26 @@
 import { styled } from "styled-components";
 import MyPageTopInfo from "./MyPageTopInfo";
 import Responsive from "../../containers/common/Responsive";
+import { useDispatch, useSelector } from "react-redux";
+import { listEvents } from "../../modules/eventlist";
+import { useEffect } from "react";
 
-const MyPageMain = () => {
+const MyPageMain = ({ user, loading }) => {
+  const dispatch = useDispatch();
+  const events = useSelector((state) => ({
+    eventlist: state.eventlist.event || [],
+  }));
+  useEffect(() => {
+    dispatch(listEvents());
+  }, [dispatch]);
+  console.log("eventlist", events);
   return (
     <MyPageMainBlock>
-      <MyPageTopInfo />
+      <MyPageTopInfo
+        user={user}
+        loading={loading}
+        eventlist={events.eventlist}
+      />
     </MyPageMainBlock>
   );
 };
